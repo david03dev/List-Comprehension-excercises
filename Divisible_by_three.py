6b18ebe5e2a8e51a2e41672e8d897d912d521ab1
David Selvaraj <166501046+david03dev@users.noreply.github.com>
2024-11-21 12:01:18 +0530

    Update Divisible_by_three.py

--- Divisible_by_three.py
+++ Divisible_by_three.py
@@ -4,7 +4,9 @@
 
 #Initialize given list
 given_list = [3, 6, 9, 12, 15, 18, 20, 21, 25]
+
 #generate a new list of numbers divisible by 3.
 list_of_3_divisions = [x for x in given_list if x % 3 == 0]
+
 #print the results
 print(list_of_3_divisions)
